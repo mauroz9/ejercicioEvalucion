2ad8e41a0de222e0419b80ba70536ca0730af278
mauroz9 <mauro.serranoo26@gmail.com>
2025-11-21 09:07:10 +0100

    inicialización de proyecto

---
+++ src/test/java/com/salesianostriana/dam/ejercicioevaluacion/EjercicioevaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicioevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioevaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
